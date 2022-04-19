f16e587a47011af5f34ddd8bd98934af56ba9b05
Refuze <refuzeapple@gmail.com>
2022-04-19 12:51:54 +0300

    First commit

---
+++ src/test/java/com/learning/springblogwebapp/SpringBlogWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springblogwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
